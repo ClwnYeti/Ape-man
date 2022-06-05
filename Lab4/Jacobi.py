faa8dd6ecc313a21dae883cddf4e00a0efab9582
kutuzovmikv <kutuzovmikv@gmail.com>
2022-06-05 15:59:32 +0300

    feat: did all dirty work

--- Lab4/Jacobi.py
+++ Lab4/Jacobi.py
@@ -1,59 +1,49 @@
-from numpy import array, identity, diagonal
-from math import sqrt
+import numpy as np
+from numpy import ndarray
+from math import *
 
 
-def jacobi(a, tol=1.0e-9):  # Jacobi method
+def jacobi(a, accuracy=0.01):  # Jacobi method
 
-    def maxElem(a):  # Find largest off-diag. element a[k,l]
+    def maxElem(a: ndarray):  # Find largest off-diag. element a[max_i,max_j]
         n = len(a)
-        aMax = 0.0
+        aMax = 0
+        max_i = max_j = -1
         for i in range(n - 1):
             for j in range(i + 1, n):
-                if abs(a[i][j]) >= aMax:
+                if abs(a[i][j]) > aMax:
                     aMax = abs(a[i][j])
-                    k = i
-                    l = j
-        return aMax, k, l
+                    max_i = i
+                    max_j = j
+        return aMax, max_i, max_j
 
-    def rotate(a, p, k, l):  # Rotate to make a[k,l] = 0
-        n = len(a)
-        aDiff = a[l][l] - a[k][k]
-        if abs(a[k][l]) < abs(aDiff) * 1.0e-36:
-            t = a[k][l] / aDiff
-        else:
-            phi = aDiff / (2.0 * a[k][l])
-            t = 1.0 / (abs(phi) + sqrt(phi ** 2 + 1.0))
-            if phi < 0.0: t = -t
-        c = 1.0 / sqrt(t ** 2 + 1.0)
-        s = t * c
-        tau = s / (1.0 + c)
-        temp = a[k][l]
-        a[k][l] = 0.0
-        a[k][k] = a[k][k] - t * temp
-        a[l][l] = a[l][l] + t * temp
-        for i in range(k):  # Case of i < k
-            temp = a[i][k]
-            a[i][k] = temp - s * (a[i][l] + tau * temp)
-            a[i][l] = a[i][l] + s * (temp - tau * a[i][l])
-        for i in range(k + 1, l):  # Case of k < i < l
-            temp = a[k][i]
-            a[k][i] = temp - s * (a[i][l] + tau * a[k][i])
-            a[i][l] = a[i][l] + s * (temp - tau * a[i][l])
-        for i in range(l + 1, n):  # Case of i > l
-            temp = a[k][i]
-            a[k][i] = temp - s * (a[l][i] + tau * temp)
-            a[l][i] = a[l][i] + s * (temp - tau * a[l][i])
-        for i in range(n):  # Update transformation matrix
-            temp = p[i][k]
-            p[i][k] = temp - s * (p[i][l] + tau * p[i][k])
-            p[i][l] = p[i][l] + s * (temp - tau * p[i][l])
+    def rotate(matrix, i, j, n):  # Rotate to make a[max_i,max_j] = 0
+        rotation_matrix = np.eye(n)
+        phi = 1 / 2 * atan((2 * matrix[i][j]) / (matrix[i][i] - matrix[j][j]))
+
+        rotation_matrix[i][j] = -sin(phi)
+        rotation_matrix[j][i] = sin(phi)
+        rotation_matrix[i][i] = cos(phi)
+        rotation_matrix[j][j] = cos(phi)
+
+        return rotation_matrix
 
     n = len(a)
-    maxRot = 5 * (n ** 2)  # Set limit on number of rotations
-    p = identity(n) * 1.0  # Initialize transformation matrix
-    for i in range(maxRot):  # Jacobi rotation loop
-        aMax, k, l = maxElem(a)
-        if aMax < tol:
-            return diagonal(a), p
-        rotate(a, p, k, l)
-    print('Jacobi method did not converge')
+    iterations = 0
+    matrix_copy = a.copy()
+    vectors = np.eye(n)  # Initialize transformation matrix
+
+    while 1:  # Jacobi rotation loop
+        max_element, max_i, max_j = maxElem(matrix_copy)
+
+        if max_element < accuracy:
+            print(iterations)
+            vectors = ndarray.tolist(vectors.T)
+            eigenvalues = ndarray.tolist(matrix_copy.diagonal())
+            return eigenvalues, vectors
+
+        iterations += 1
+        transformation_matrix = rotate(matrix_copy, max_i, max_j, n)
+        vectors = vectors.dot(transformation_matrix)
+
+        matrix_copy = transformation_matrix.T.dot(matrix_copy).dot(transformation_matrix)
--- Lab4/Result.py
+++ Lab4/Result.py
@@ -1,3 +1,7 @@
+import pprint
+
+import numpy as np
+
 from Jacobi import jacobi
 
 
@@ -5,7 +9,39 @@ def hilbert(k):
     return [[1 / (i + j + 1) for j in range(k)] for i in range(k)]
 
 
+def random_simmetric_matrix(n: int) -> np.array:
+    matrix = np.random.randint(-1000, 1000, size=(n, n))
+
+    for i in range(n):
+        for j in range(n):
+            if i > j:
+                matrix[i][j] = matrix[j][i]
+
+    return matrix
+
+
+def symmetrical_matrix_with_diagonal_dominance(k, n):
+    matrix = np.random.uniform(-n ** 3, n ** 3, size=(n, n))
+    for i in range(n):
+        if matrix[i][i] == 0:
+            matrix[i][i] = np.random.randint(1, n ** 3)
+
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                matrix[i][j] = matrix[i][i] / (k * 1.0) * np.random.randint(-n ** 2, n ** 2 + 1)
+        matrix[i][i] *= k ** 2
+
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                matrix[i][j] = min(abs(matrix[i][j]), abs(matrix[j][i]))
+                matrix[j][i] = matrix[i][j]
+
+    return matrix
+
+
 A = hilbert(3)
 
-print(A)
-print(jacobi(A))
+pprint.pprint(A)
+pprint.pprint(jacobi(A))
